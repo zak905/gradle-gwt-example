8b592dd006e3305fd0288746612ff4d2d4f61012
zak905 <zakaria.amine88@gmail.com>
2017-01-18 20:24:36 +0100

    initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class App implements EntryPoint {
+	
+	public void onModuleLoad() {
+	
+
+	}
+}
---
+++ src/test/java/com/example/AppTests.java
@@ -0,0 +1,14 @@
+package com.gwidgets.temp.client;
+
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.junit.client.GWTTestCase;
+
+
+public class AppTests extends GWTTestCase {
+
+	public String getModuleName() {
+		return "module";
+	}
+
+}
